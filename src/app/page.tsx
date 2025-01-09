9b4166f5f45e0314ed7a2e71d49410fa6c0409fd
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-01-09 15:31:18 +0100

    added VideoCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { VideoCard } from '@/ui/video-card/VideoCard'
+import { videoService } from '@/services/video.service'
+
 export default function Home() {
-	return <h1>Home page</h1>
+	const { data, isLoading } = useQuery({
+		queryKey: ['explore'],
+		queryFn: () => videoService.getExploreVideos()
+	})
+
+	return (
+		<div>
+			{isLoading ? (
+				'Loading...'
+			) : data?.data.videos.length ? (
+				data.data.videos.map(video => (
+					<VideoCard
+						key={video.id}
+						video={video}
+					/>
+				))
+			) : (
+				<div>Error fetching data</div>
+			)}
+		</div>
+	)
 }
