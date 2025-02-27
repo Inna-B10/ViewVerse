294f78545151ac02d144d449be28fb08d998d5f8
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-02-27 14:36:23 +0100

    "Like" button functionality

--- src/app/(public)/video/[publicId]/video-actions/VideoActions.tsx
+++ src/app/(public)/video/[publicId]/video-actions/VideoActions.tsx
@@ -1,8 +1,48 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import cn from 'clsx'
 import { Heart, ListPlus } from 'lucide-react'
+import { startTransition, useEffect, useState } from 'react'
+import { useProfile } from '@/hooks/useProfile'
 import { transformCount } from '@/utils/transform-count'
+import { userService } from '@/services/user.service'
 import type { ISingleVideoResponse } from '@/types/video.types'
 
 export function VideoActions({ video }: { video: ISingleVideoResponse }) {
+	const { profile, refetch } = useProfile()
+	const isLiked = profile?.likes.some(like => like.videoId === video.id) || false
+
+	const [isLikedLocal, setIsLikedLocal] = useState(isLiked)
+
+	const [optimisticLike, setOptimisticLike] = useState<number>(video.likes.length)
+
+	useEffect(() => {
+		setIsLikedLocal(isLiked)
+	}, [isLiked])
+
+	const { mutate } = useMutation({
+		mutationKey: ['like', video.id],
+		mutationFn: () => userService.toggleLike(video.id),
+		onMutate() {
+			startTransition(() => {
+				const newIsLiked = !isLikedLocal
+				setIsLikedLocal(newIsLiked)
+				setOptimisticLike(prev => (newIsLiked ? prev + 1 : prev - 1))
+			})
+		},
+		onError() {
+			startTransition(() => {
+				const revertedIsLiked = !isLikedLocal
+				setIsLikedLocal(revertedIsLiked)
+				setOptimisticLike(prev => (revertedIsLiked ? prev + 1 : prev - 1))
+			})
+		},
+		onSuccess() {
+			refetch()
+		}
+	})
+
 	return (
 		<div className='flex justify-end items-center gap-5'>
 			<button
@@ -15,9 +55,13 @@ export function VideoActions({ video }: { video: ISingleVideoResponse }) {
 			<button
 				className='text-primary text-lg flex items-center gap-1.5 transition-opacity opacity-80 hover:opacity-100'
 				title='Likes'
+				onClick={() => mutate()}
 			>
-				<Heart size={19} />
-				{transformCount(video.likes.length)}
+				<Heart
+					size={19}
+					className={cn('text-primary', { 'fill-primaryDark': isLiked })}
+				/>
+				{transformCount(optimisticLike)}
 			</button>
 		</div>
 	)
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,12 +5,19 @@ import type { IProfileResponse } from '@/types/user.types'
 class UserService {
 	private _USERS = '/users'
 
+	/* ------------------------------- GetProfile ------------------------------- */
 	async getProfile() {
 		const { data } = await instance.get<IProfileResponse>(`${this._USERS}/profile`)
 		return data
 	}
+	/* ------------------------------ UpdateProfile ----------------------------- */
 	async updateProfile(settings: ISettingsData) {
 		const { data } = await instance.put<boolean>(`${this._USERS}/profile`, settings)
+		return data
+	}
+	/* ------------------------------- ToggleLike ------------------------------- */
+	async toggleLike(videoId: string) {
+		const { data } = await instance.put(`${this._USERS}/profile/likes`, { videoId })
 		return data
 	}
 }
