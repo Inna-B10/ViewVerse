6a75ba9e825b721b811ae5dd1b7e9c436305f1e4
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-05-20 10:58:28 +0200

    feat: implement infinite scrolling in Explore component; add useEffectScroll hook and create ExploreSection for better structure

--- src/app/explore/Explore.tsx
+++ src/app/explore/Explore.tsx
@@ -1,28 +1,52 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { SkeletonLoader } from '@/ui/SkeletonLoader'
 import { VideoCard } from '@/ui/video-card/VideoCard'
 import { useAuth } from '@/hooks/useAuth'
+import { useEffectScroll } from '@/hooks/useEffectScroll'
 import { videoService } from '@/services/video.service'
 
 export function Explore() {
 	const { user } = useAuth()
 
-	const { data, isLoading } = useQuery({
+	const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery({
 		queryKey: ['explore'],
-		queryFn: () => videoService.getExploreVideos(user?.id)
+		queryFn: ({ pageParam }) =>
+			videoService.getExploreVideos(
+				user?.id,
+				{
+					page: pageParam.page,
+					limit: 12
+				},
+				pageParam.excludeIds
+			),
+		initialPageParam: { page: 1, excludeIds: [] as string[] },
+		getNextPageParam: (lastPage, allPages) => {
+			const { page, totalPages } = lastPage
+			const allVideoIds = allPages.flatMap(page => page.videos.map(video => video.id))
+
+			return page < totalPages ? { page: page + 1, excludeIds: allVideoIds } : undefined
+		}
 	})
 
+	useEffectScroll({
+		fetchNextPage,
+		hasNextPage,
+		isFetchingNextPage
+	})
+
+	const allVideos = data?.pages.flatMap(page => page.videos) || []
+
 	return (
-		<div className='grid-cols'>
-			{isLoading ? (
+		<section className='grid-cols'>
+			{isLoading && !allVideos.length ? (
 				<SkeletonLoader
 					count={5}
 					className='h-36 rounded-md'
 				/>
-			) : data?.videos?.length ? (
-				data.videos.map(video => (
+			) : allVideos?.length ? (
+				allVideos.map(video => (
 					<VideoCard
 						key={video.id}
 						video={video}
@@ -33,6 +57,13 @@ export function Explore() {
 					<p>Explore videos are temporarily unavailable.</p>
 				</div>
 			)}
-		</div>
+
+			{isFetchingNextPage && (
+				<SkeletonLoader
+					count={5}
+					className='h-36 rounded-md'
+				/>
+			)}
+		</section>
 	)
 }
