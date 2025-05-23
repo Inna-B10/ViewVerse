bf48c92d035b7da810edb98c67810de942efb045
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-05-23 14:42:23 +0200

    feat: improve video loading experience with skeleton loader and error handling

--- src/components/ui/video-player/VideoPlayer.tsx
+++ src/components/ui/video-player/VideoPlayer.tsx
@@ -1,10 +1,17 @@
 'use client'
 
-import { Lightbulb, LightbulbOff, Maximize, Pause, Play, RectangleHorizontal } from 'lucide-react'
+import {
+	Lightbulb,
+	LightbulbOff,
+	Maximize,
+	Pause,
+	Play,
+	RectangleHorizontal,
+	VideoOff
+} from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
 import { useUpdateViews } from '@/hooks/useUpdateViews'
 import { findPrimaryAndBackgroundVideo } from '@/utils/findMainAndBackgroundVideo'
-import { SkeletonLoader } from '../SkeletonLoader'
 import { PlayerProgressBar } from './progress-bar/PlayerProgressBar'
 import { SelectQuality } from './quality/SelectQuality'
 import { useVideoPlayer } from './use-video-player/useVideoPlayer'
@@ -22,6 +29,8 @@ interface Props {
 
 export function VideoPlayer({ video, isVideoOwner, toggleTheaterMode, maxResolution }: Props) {
 	const fileName = video.videoFileName
+	const [isVideoReady, setIsVideoReady] = useState(false)
+	const [isLoading, setIsLoading] = useState(true)
 
 	/* ------------------- Checking The Existing Video Quality ------------------ */
 	const [mainSrc, setMainSrc] = useState<string | null>(null)
@@ -30,11 +39,13 @@ export function VideoPlayer({ video, isVideoOwner, toggleTheaterMode, maxResolut
 
 	useEffect(() => {
 		async function loadVideos() {
+			setIsLoading(true)
 			const { primaryVideo, backgroundVideo, primaryQuality } =
 				await findPrimaryAndBackgroundVideo(fileName)
 			setMainSrc(primaryVideo)
 			setBgSrc(backgroundVideo)
 			setMainQuality(primaryQuality)
+			setIsLoading(false)
 		}
 		loadVideos()
 	}, [fileName])
@@ -49,35 +60,59 @@ export function VideoPlayer({ video, isVideoOwner, toggleTheaterMode, maxResolut
 
 	const hasPlayedRef = useRef(false)
 
-	return (
-		<div className='relative rounded-2xl w-fit mx-auto'>
-			{mainQuality ? (
-				<>
-					{state.isBacklightMode && bgSrc && (
-						<video
-							ref={bgRef}
-							className='absolute top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 w-full object-center object-cover filter blur-3xl scale-[1.02] brightness-90 contrast-125 saturate-150 rounded-xl'
-							src={bgSrc}
-							muted
-						/>
-					)}
-					<video
-						ref={playerRef}
-						onPlay={() => {
-							if (!isVideoOwner && !hasPlayedRef.current) {
-								hasPlayedRef.current = true
-								runUpdateViews() // safe hook call
-							}
-						}}
-						onEnded={() => {
-							fn.setIsPlaying(false)
-						}}
-						className='aspect-video width-full mx-auto relative z-[1] rounded-xl'
-						controls={false}
-						src={mainSrc}
-						preload='metadata'
-					/>
+	{
+		/* Skeleton while video is loading */
+	}
+	if (isLoading) {
+		return (
+			<div className='aspect-video relative -top-full bg-field border border-border rounded-md animate-pulse flex items-center justify-center'>
+				<p className='text-lg text-muted-foreground'>Video loading...</p>
+			</div>
+		)
+	}
 
+	if (!mainQuality) {
+		return (
+			<div className='aspect-video bg-field border border-border rounded-md flex flex-col gap-4 items-center justify-center text-2xl'>
+				<VideoOff size={40} />
+				<p>Error loading media: </p>
+				<p>File Could Not Be Played</p>
+			</div>
+		)
+	}
+
+	return (
+		<div className='aspect-video relative rounded-2xl mx-auto '>
+			<>
+				{state.isBacklightMode && bgSrc && (
+					<video
+						ref={bgRef}
+						className='absolute top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 w-full object-center object-cover filter blur-3xl scale-[1.02] brightness-90 contrast-125 saturate-150 rounded-xl'
+						src={bgSrc}
+						muted
+					/>
+				)}
+				<video
+					ref={playerRef}
+					onCanPlay={() => setIsVideoReady(true)}
+					onPlay={() => {
+						if (!isVideoOwner && !hasPlayedRef.current) {
+							hasPlayedRef.current = true
+							runUpdateViews() // safe hook call
+						}
+					}}
+					onEnded={() => {
+						fn.setIsPlaying(false)
+					}}
+					className={`relative top-0 left-0 w-full h-full rounded-xl z-[1] transition-opacity duration-300 ${
+						isVideoReady ? 'opacity-100' : 'opacity-0'
+					}`}
+					controls={false}
+					src={mainSrc ?? undefined}
+					preload='metadata'
+				/>
+
+				{isVideoReady && (
 					<div className='grid grid-cols-[7fr_1fr] gap-7 absolute bottom-5 left-5 right-5 z-[1] py-1  px-2 hover:rounded hover:bg-[#9c9c9c70] transition-all duration-300'>
 						<div className='flex items-center gap-4 w-full'>
 							{/* -------------------------------- Btn Play -------------------------------- */}
@@ -139,16 +174,8 @@ export function VideoPlayer({ video, isVideoOwner, toggleTheaterMode, maxResolut
 							</button>
 						</div>
 					</div>
-				</>
-			) : (
-				// <div className='w-full h-[80svh] bg-field border border-border rounded-md flex flex-col gap-4 items-center justify-center text-2xl'>
-				// 	<VideoOff size={40} />
-				// 	<p>Error loading media: </p>
-				// 	<p>File Could Not Be Played</p>
-				// </div>
-				//[TODO] доработать загрузку плеера на VideoPlayer
-				<SkeletonLoader className='h-[80svh] w-full' />
-			)}
+				)}
+			</>
 		</div>
 	)
 }
