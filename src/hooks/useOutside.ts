428dc89183194546981ee82e118f37b62a920390
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-06-02 13:33:52 +0200

    refactor: optimize handleClickOutside function using useCallback for better performance

--- src/hooks/useOutside.ts
+++ src/hooks/useOutside.ts
@@ -1,4 +1,4 @@
-import { type Dispatch, type SetStateAction, useEffect, useRef, useState } from 'react'
+import { type Dispatch, type SetStateAction, useCallback, useEffect, useRef, useState } from 'react'
 
 type TypeOut = {
 	ref: React.RefObject<any | null>
@@ -10,19 +10,22 @@ export const useOutside = (initialIsVisible: boolean, onClose?: () => void): Typ
 	const [isShow, setIsShow] = useState(initialIsVisible)
 	const ref = useRef<HTMLElement>(null)
 
-	const handleClickOutside = (event: MouseEvent) => {
-		if (ref.current && !ref.current.contains(event.target as Node)) {
-			setIsShow(false)
-			onClose?.()
-		}
-	}
+	const handleClickOutside = useCallback(
+		(event: MouseEvent) => {
+			if (ref.current && !ref.current.contains(event.target as Node)) {
+				setIsShow(false)
+				onClose?.()
+			}
+		},
+		[ref, onClose]
+	)
 
 	useEffect(() => {
 		document.addEventListener('click', handleClickOutside, true)
 		return () => {
 			document.removeEventListener('click', handleClickOutside, true)
 		}
-	}, [])
+	}, [handleClickOutside])
 
 	return { ref, isShow, setIsShow }
 }
