88bf02e8781bd475f34a3c6c449eaf088e3a1539
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-06-09 05:22:22 +0200

    feat: added bundle analyzer and update package scripts for analysis

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,30 @@
+// import type { NextConfig } from 'next'
+//
+// const nextConfig: NextConfig = {
+// 	reactStrictMode: true,
+// 	poweredByHeader: false,
+// 	async rewrites() {
+// 		return [
+// 			{
+// 				source: '/uploads/:path*',
+// 				destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/uploads/:path*`
+// 			}
+// 		]
+// 	},
+// 	eslint: {
+// 		// Warning: This allows production builds to successfully complete even if
+// 		// your project has ESLint errors.
+// 		ignoreDuringBuilds: true
+// 	}
+// }
+//
+// export default nextConfig
+import createAnalyzer from '@next/bundle-analyzer'
 import type { NextConfig } from 'next'
+
+const withBundleAnalyzer = createAnalyzer({
+	enabled: process.env.ANALYZE === 'true'
+})
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
@@ -12,10 +38,9 @@ const nextConfig: NextConfig = {
 		]
 	},
 	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
 		ignoreDuringBuilds: true
 	}
 }
 
-export default nextConfig
+// ⬇️ Экспорт с обёрткой анализа
+export default withBundleAnalyzer(nextConfig)
